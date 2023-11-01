9831fec092cc6112cb2c561b8928c8efbe6c6452
jwehr5 <113799010+jwehr5@users.noreply.github.com>
2023-11-01 11:59:46 -0500

    Added the cryptocurrency logo

--- src/Crypto.js
+++ src/Crypto.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import './Crypto.css';
 import Coin_graph from './imgs/Coin_graph.png';
+import Currency_logo from './imgs/DZ_currency_logo_ver_2.png';
 
 function Crypto() {
     return (
         <div className="crypto-page">
-            <h1>DecentraZoo Crypto</h1>
-            <p>Welcome to the DecentraZoo Crypto page. Here, you can view the latest updates about our in-game currency, $DZ, its market value, and more!</p>
+            <div className="page-title-and-logo">
+                <h1>DecentraZoo Crypto (DZ)</h1>
+                <img src={Currency_logo} width={50} height={50}/>
+            </div>
+            <p>Welcome to the DecentraZoo Crypto page. Here, you can view the latest updates about our in-game currency, DZ, its market value, and more!</p>
             {/* Add more sections as needed */}
              <br></br>
-            <h1>$DZ Stats</h1>
+            <h1>DZ Stats</h1>
             <div className='Coingraph'>
                 <div>
-                    <p>Price of $DZ as of 10/27/2023</p>
+                    <p>Price of DZ as of 10/27/2023</p>
                     <img src={Coin_graph}/>
                 </div>
                 <div className='Coinstats'>
@@ -24,8 +28,8 @@ function Crypto() {
             </div>
 
             <div className='Distribution'>
-                <h1>Distribution of $DZ</h1>
-                <b>Total Number of $DZ tokens that have been minted: 4,000,000,000</b>
+                <h1>Distribution of DZ</h1>
+                <b>Total Number of DZ tokens that have been minted: 4,000,000,000</b>
                 <ul>
                     <li>2,000,000,000 has been set aside for the game wallet</li>
                     <br></br>
